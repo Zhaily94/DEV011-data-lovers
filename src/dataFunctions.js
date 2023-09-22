2e1248b35ed2ca66c4d97e290015571dc24c9212
Zhaira Lysset Santos Alfaro <52017903+Zhaily94@users.noreply.github.com>
2023-09-21 23:17:13 -0600

    Se agregaron atributos,caracteristicas a card

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -17,33 +17,55 @@ export const request = (url, name) => {
 export const createPokemon = (pokemon) => {
   if(pokemon){
     const pokemonLista = document.querySelector(".lista");
-    const card = document.createElement('li');
-    card.classList.add('tarjeta-pokemon');
+    const li = document.createElement('li');
+    li.classList.add('tarjeta-pokemon');
   
-    const tarjeta = document.createElement('div');
-    tarjeta.classList.add('card');
+    const card = document.createElement('div');
+    card.classList.add('card');
   
-    const imagen = document.createElement('img');
-    imagen.src = pokemon.sprites.front_default;
-    tarjeta.appendChild(imagen);
-    card.appendChild(tarjeta);
+    const container = document.createElement('dl');
+    container.setAttribute('itemscope', '');
+    container.setAttribute('itemtype', 'pokemon');
+    container.classList.add('container');
+    card.appendChild(container);
+
+    const img = document.createElement('img');
+    img.src = pokemon.sprites.front_default;
+    container.appendChild(img);
+   
+    const nom = document.createElement('dt');
+    nom.textContent = "Name: ";
+    container.appendChild(nom);
+
   
-    const caracteristicas = document.createElement('div');
-    caracteristicas.classList.add('container');
-    card.appendChild(caracteristicas);
+    const name = document.createElement('dd');
+    name.setAttribute('itemprop', 'name');
+    name.classList.add('class-nombre'); 
+    name.textContent = pokemon.name;
+    container.appendChild(name);
+
+    const num = document.createElement('dt');
+    num.textContent = "Num: ";
+    container.appendChild(num);
+
+    const id = document.createElement('dd');
+    id.setAttribute('itemprop', 'num');
+    id.classList.add('class-id'); 
+    id.textContent = "00"+pokemon.id;
+    container.appendChild(id);
   
-    const nombre = document.createElement('h4');
-    nombre.classList.add('class-nombre'); 
-    nombre.textContent = pokemon.nombre;
-    caracteristicas.appendChild(nombre);
+    const typ = document.createElement('dt');
+    typ.textContent = "Type: ";
+    container.appendChild(typ);
+
+    const type = document.createElement('dd');
+    type.setAttribute('itemprop', 'type');
+    type.classList.add('class-tipo');
+    type.textContent = pokemon.types[0].type.name;
+    container.appendChild(type);
   
-  
-  
-    const tipo = document.createElement('p');
-    tipo.classList.add('class-tipo');
-    caracteristicas.appendChild(tipo);
-  
-    pokemonLista.appendChild(card);
+    li.appendChild(card);
+    pokemonLista.appendChild(li);
   
   }
 
